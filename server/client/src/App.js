88eaee1f37e1687c3f06e9ecdf3d49007436989a
WaylayOvercast <timelyovercast@gmail.com>
2022-07-09 14:34:03 -0500

    some styling

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -4,6 +4,7 @@ import Feed from './components/Feed';
 import apolloImage from './images/apollo-blog.png';
 
 function App() {
+  const [toggle, setToggle] = React.useState(false);
 
 
   return (
@@ -11,11 +12,34 @@ function App() {
       <nav className='nav'>
         <img src={apolloImage}></img>
       </nav>
-      <section className='feed'>
-        <Feed>
+      
+      {
+        toggle ?
+        <section className='feed'>
+          <Feed>
 
-        </Feed>
-      </section>
+          </Feed>
+        </section> 
+        :
+        <div className='disclaim'>
+          <div className='text-box'>
+            <p>
+              Hello and welcome to my testing blog. 
+            </p>
+            <p>
+              This is just a small website I created to better understand Apollo, Graphql and Prisma.
+            </p>
+            <p>
+              Thanks. -Max
+            </p>
+          </div>
+          <button 
+            onClick={() => setToggle(true)}
+            className='loadFeed'
+            >Click here to load feed
+          </button>
+        </div>
+      }
     </div>
   );
 }
